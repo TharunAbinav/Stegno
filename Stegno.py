8abd02c1a63bac1abaddbc15101bfc0e6cfe49c2
THARUN ABINAV S <109855807+TharunAbinav@users.noreply.github.com>
2026-06-16 10:15:40 +0530

    Added image selection UI
    -add img selecting using tkinter for better intractability than selecting address

--- Stegno.py
+++ Stegno.py
@@ -1,5 +1,23 @@
 '''This Project is about using Quantum key to secure the data 
 and then using Steganography to hide the data in an image.'''
 import qiskit
-import random
+import tkinter as tk
+from tkinter import filedialog
 from PIL import Image
+
+root=tk.Tk()
+root.withdraw()
+
+print("Opening file section")
+file_path = filedialog.askopenfilename(
+    title="Select an Image",
+    filetypes=[("Image Files", "*.jpg *.jpeg *.png *.bmp *.webp")]
+)
+
+if file_path:
+    img = Image.open(file_path)
+    print(f"Successfully selected: {file_path}")
+    print(f"Image Size: {img.size}")
+    img.show()
+else:
+    print("User canceled image selection.")
